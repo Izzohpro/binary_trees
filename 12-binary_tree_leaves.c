638e276d5b049e55104f0877d11cfdd18b0cf4af
TeeJude <teejayjude@gmail.com>
2023-08-31 12:10:04 -0700

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,7 +6,7 @@
  * Returns: 0 if tree is NULL
  */
 
-size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaf = 0, l = 0, r = 0;
 
