fee952588a28e2e2377b4bc31f480a52e9075dfa
TeeJude <teejayjude@gmail.com>
2023-08-31 11:47:46 -0700

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -17,8 +17,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	else
 	{
 		node += ((tree->left || tree->right) ? 1 : 0);
-		node += binary_tree_nodes(tree->left);
 		node += binary_tree_nodes(tree->right);
+		node += binary_tree_nodes(tree->left);
 		return (node);
 	}
 }
