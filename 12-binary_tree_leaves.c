3b3a83ce90b19e63d22267c8e9c9a7d0de6260e2
TeeJude <teejayjude@gmail.com>
2023-08-31 12:07:18 -0700

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,7 +8,7 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree);
 {
-	size_t leaf = 0, r = 0, l = 0;
+	size_t leaf = 0, l = 0, r = 0;
 
 	if (tree == NULL)
 	{
@@ -16,10 +16,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 	}
 	else
 	{
-		r = binary_tree_leaves(tree->right);
 		l = binary_tree_leaves(tree->left);
+		r = binary_tree_leaves(tree->right);
 		leaf = l + r;
 		return ((!l && !r) ? leaf + 1 : leaf + 0);
 	}
 }
-
