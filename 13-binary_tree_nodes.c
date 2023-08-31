6f010ada3fff49a56b8389b7841b0d93743f3142
TeeJude <teejayjude@gmail.com>
2023-08-31 11:44:55 -0700

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_nodes - function that counts the nodes with at 
  * 		       least 1 child in a binary tree
- * @tree: tree to check
+ * @tree: pointer to the rootnode of the tree to count number of nodes
  * Return: number of nodes with children
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
@@ -17,9 +17,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	else
 	{
 		node += ((tree->left || tree->right) ? 1 : 0);
-		node += binary_tree_nodes(tree->right);
 		node += binary_tree_nodes(tree->left);
+		node += binary_tree_nodes(tree->right);
 		return (node);
 	}
 }
-
